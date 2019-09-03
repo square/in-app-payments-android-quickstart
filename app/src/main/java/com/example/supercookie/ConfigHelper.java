7923a5f8c614b9661674eca643f88edbb64219f4
Shannon Skipper <shannonskipper@gmail.com>
2019-09-03 13:38:18 -0700

    Switch Google Pay ID to be a Merchant ID

--- app/src/main/java/com/example/supercookie/ConfigHelper.java
+++ app/src/main/java/com/example/supercookie/ConfigHelper.java
@@ -7,7 +7,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class ConfigHelper {
 
-  public static final String SQUARE_LOCATION_ID_FOR_GOOGLE_PAY = "REPLACE_ME";
+  public static final String GOOGLE_PAY_MERCHANT_ID = "REPLACE_ME";
   private static final String CHARGE_SERVER_HOST = "REPLACE_ME";
   private static final String CHARGE_SERVER_URL = "https://" + CHARGE_SERVER_HOST + "/";
 
@@ -15,8 +15,8 @@ public class ConfigHelper {
     return !CHARGE_SERVER_HOST.equals("REPLACE_ME");
   }
 
-  public static boolean squareLocationIdSet() {
-    return !SQUARE_LOCATION_ID_FOR_GOOGLE_PAY.equals("REPLACE_ME");
+  public static boolean merchantIdSet() {
+    return !GOOGLE_PAY_MERCHANT_ID.equals("REPLACE_ME");
   }
 
   public static void printCurlCommand(String nonce) {
